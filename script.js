bdb5594fe1a886fbe391f66f7db186381c94d31a
Shamsullah Ahmadzai <70674350+SHAMSK123@users.noreply.github.com>
2021-08-14 22:07:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function Random() {
+    return Math.floor(Math.random("") * 101);
+    randomValue = function() {};
+};
+
+function randomValue() {
+    document.getElementById('randnum').value = Random();
+};
